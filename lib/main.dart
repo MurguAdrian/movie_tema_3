34ea5e7b5c69eb9b1ba0fc426e3947b3f9bc35c4
Home <murgu.adrian95@gmail.com>
2023-03-11 09:41:42 +0200

    V1

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MoviePageState extends State<MoviePage> {
   bool isLoading = false;
   final List<String> _title = <String>[];
   final List<String> _img = <String>[];
-  final List<int> _years = <int> [];
+  final List<int> _years = <int>[];
   final List<int> _rate = <int>[];
 
   void _getMovie() {
@@ -50,8 +50,8 @@ class _MoviePageState extends State<MoviePage> {
       setState(() {
         _title.addAll(movies.map((dynamic item) => item["title"] as String));
         _img.addAll(movies.map((dynamic item) => item["medium_cover_image"] as String));
-        _years.addAll(movies.map((dynamic item) => item ["year"] as int));
-        _rate.addAll(movies.map((dynamic item) => item ["runtime"] as int));
+        _years.addAll(movies.map((dynamic item) => item["year"] as int));
+        _rate.addAll(movies.map((dynamic item) => item["runtime"] as int));
         isLoading = false;
       });
     });
@@ -61,81 +61,92 @@ class _MoviePageState extends State<MoviePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Builder(builder: (BuildContext) {
-        if (isLoading) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return ListView.builder(
-          itemCount: _title.length,
-          itemBuilder: (BuildContext, int index) {
-            final String title = _title[index];
-            final String img = _img[index];
-            final int years = _years[index];
-            final int rate = _rate[index];
-
-            return Builder(
-              builder: (context) {
-                return Row(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.all(20),
-                          alignment: Alignment.centerLeft,
-                          height: 200,
-                          width: 150,
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            image: DecorationImage(image: NetworkImage(img), fit: BoxFit.cover),borderRadius: BorderRadius.circular(20)
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 50,
-                          width: 200,
-                          child: Text(
-                            title,
-                            maxLines: 2,style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),textAlign: TextAlign.center,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 50,
-                          width: 200,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-
-                              Column(
-                                children: [
-                                  Text('Anul '),
-                                  Text('$years',style: TextStyle(fontSize: 18,letterSpacing: 10),),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text('Runtime'),
-                                  Text('$rate',style: TextStyle(fontSize: 15,)),
-                                ],
-                              ),
-
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              },
+      body: Builder(
+        builder: (BuildContext) {
+          if (isLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        );
-      }),
+          }
+          return ListView.builder(
+            itemCount: _title.length,
+            itemBuilder: (BuildContext, int index) {
+              final String title = _title[index];
+              final String img = _img[index];
+              final int years = _years[index];
+              final int rate = _rate[index];
+
+              return Builder(
+                builder: (context) {
+                  return Row(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(20),
+                            alignment: Alignment.centerLeft,
+                            height: 200,
+                            width: 150,
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(image: NetworkImage(img), fit: BoxFit.cover),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 50,
+                            width: 200,
+                            child: Text(
+                              title,
+                              maxLines: 2,
+                              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                            width: 200,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text('Anul '),
+                                    Text(
+                                      '$years',
+                                      style: TextStyle(fontSize: 18, letterSpacing: 10),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text('Runtime'),
+                                    Text(
+                                      '$rate',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
